c754c1531fbdfacf85c32c0352a4bf973c6e6ca4
kaingi <kaingidaudi@gmail.com>
2025-08-07 08:55:00 +0300

    Initial commit

---
+++ alx_travel_app/urls.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, include
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Travel API",
+      default_version='v1',
+      description="API documentation for Travel app",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('api/', include('listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
